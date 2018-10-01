20e8f57de0d63fdbda2312db048871f18e3f580e
Maxim Kupriianov <max@kc.vc>
2018-10-01 05:00:44 +0300

    Prepare OpenZepellin ERC20 contracts.

---
+++ contracts/openzeppelin/MintableToken.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.24;
+
+import "./ERC20.sol";
+import "./Ownable.sol";
+
+
+/**
+ * @title Mintable token
+ * @dev Simple ERC20 Token example, with mintable token creation
+ * @dev Issue: * https://github.com/OpenZeppelin/zeppelin-solidity/issues/120
+ */
+contract MintableToken is StandardToken, Ownable {
+    bool public mintingFinished = false;
+
+    modifier canMint() {
+        require(!mintingFinished, "");
+        _;
+    }
+
+    /**
+     * @dev Function to mint tokens
+     * @param _to The address that will recieve the minted tokens.
+     * @param _amount The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mint(address _to, uint256 _amount) public onlyOwner canMint returns (bool) {
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        emit Mint(_to, _amount);
+        return true;
+    }
+
+    /**
+     * @dev Function to stop minting new tokens.
+     * @return True if the operation was successful.
+     */
+    function finishMinting() public onlyOwner returns (bool) {
+        mintingFinished = true;
+        emit MintFinished();
+        return true;
+    }
+
+    event Mint(address indexed to, uint256 amount);
+
+    event MintFinished();
+}
